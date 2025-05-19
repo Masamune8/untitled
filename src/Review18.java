f8a46dddbac0eb7d7403f13f9fc716d8d71e9752
241062 <241062@std.hi-joho.ac.jp>
2025-05-19 14:45:50 +0900

    復習問題　１6 17

---
+++ src/Review18.java
@@ -0,0 +1,15 @@
+public class Review18 {
+    public static void main(String[] args) {
+        int num = 5;
+        int sum = 0;
+        int i = 0;
+        while (i <= num) {
+            if (i % 2 == 1) {
+                sum += i;
+            }
+            i += 1;
+        }
+        System.out.println("結果:" + sum);
+
+    }
+}
