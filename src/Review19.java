b7c1d266af880a37dfeeae66497bcd839eff2a35
241062 <241062@std.hi-joho.ac.jp>
2025-05-21 10:01:04 +0900

    復習問題　19

---
+++ src/Review19.java
@@ -0,0 +1,13 @@
+public class Review19 {
+    public static void main(String[] args) {
+        int num = 100;
+        int sum = 0;
+        int factorial = sum;
+        while (factorial <= num) {
+            factorial *= ;
+            sum += factorial;
+        }
+        System.out.println("結果:" + factorial);
+
+    }
+}
