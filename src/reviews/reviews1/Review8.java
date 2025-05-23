c89ec74befa9fdb20e6949329dda724521ba06e5
241062 <241062@std.hi-joho.ac.jp>
2025-05-23 12:21:59 +0900

    8

---
+++ src/reviews/reviews1/Review8.java
@@ -0,0 +1,14 @@
+package reviews.reviews1;
+
+public class Review8 {
+    public static void main(String[] args) {
+        double a = 10;
+        double b = a;
+        a = b;
+        a = 10000L;
+        b = 10000L;
+        b = (int)a;
+        double f = 5.94;
+        short s = 10;
+    }
+}
